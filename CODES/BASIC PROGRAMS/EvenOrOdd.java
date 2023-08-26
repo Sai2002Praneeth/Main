6bc94e8da6b42ab48b0572e9154eba62a4326be5
Sai Praneeth <110280913+Sai2002Praneeth@users.noreply.github.com>
2023-08-26 09:36:28 +0530

    Update EvenOrOdd.java

--- CODES/BASIC PROGRAMS/EvenOrOdd.java
+++ CODES/BASIC PROGRAMS/EvenOrOdd.java
@@ -1,1 +1,12 @@
-
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter any integer value : ");
+        int num = in.nextInt();
+        if (num%2 == 0) {
+            System.out.println(num + " is even.");
+        }
+        else System.out.println(num + " is odd.");
+        in.close();
+    }
+}
