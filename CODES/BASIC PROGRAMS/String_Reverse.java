a69bf89385f9d1936b95ba38c5dceeb2e2b02f33
Sai Praneeth <110280913+Sai2002Praneeth@users.noreply.github.com>
2023-08-26 09:39:03 +0530

    Update String_Reverse.java

--- CODES/BASIC PROGRAMS/String_Reverse.java
+++ CODES/BASIC PROGRAMS/String_Reverse.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+public class String_Reverse {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a string: ");
+		String str = sc.next();
+		System.out.println("Before Reverse : "+ str);
+		String rev = "";
+		for(int i = str.length()-1; i >= 0; i--) {
+			rev += str.charAt(i);
+		}
+		System.out.println("After Reverse : "+ rev);
+		sc.close();
+	}
+}
