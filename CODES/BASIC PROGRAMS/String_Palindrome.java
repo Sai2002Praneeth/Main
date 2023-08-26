4eb1537d296304dd5aa9f030c0710726f46caa12
Sai Praneeth <110280913+Sai2002Praneeth@users.noreply.github.com>
2023-08-26 09:40:03 +0530

    Update String_Palindrome.java

--- CODES/BASIC PROGRAMS/String_Palindrome.java
+++ CODES/BASIC PROGRAMS/String_Palindrome.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class String_Palindrome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a string : ");
+		String str = sc.next(); // mom, dad
+		System.out.println("Entered string : "+ str);
+		String rev = "";
+		for(int i = str.length()-1; i >= 0; i--) {
+			rev += str.charAt(i);
+		}
+		System.out.println("Reversed string : "+ rev);
+		System.out.println(str.equals(rev));
+		sc.close();
+	}
+}
