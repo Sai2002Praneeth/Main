290e5ea296d4dbef3d69016266d2df25f479bd54
Sai Praneeth <110280913+Sai2002Praneeth@users.noreply.github.com>
2023-08-26 09:22:50 +0530

    Update Fibonacci.java

--- CODES/BASIC PROGRAMS/Fibonacci.java
+++ CODES/BASIC PROGRAMS/Fibonacci.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+	public static void main (String [] args) {		
+		long f = 0;
+		long n = 1;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter the index number you would like to print : "); 
+		int number = input.nextInt();			
+		for(int i = 0; i <= number; i++) {
+			f = f + n;
+			n = f - n;
+			System.out.print(n+" ");
+		}		
+	}
+}
 
