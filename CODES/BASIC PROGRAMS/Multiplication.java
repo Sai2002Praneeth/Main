ff8a40f531f76febc3cb636d20ecf5c929e33577
Sai Praneeth <110280913+Sai2002Praneeth@users.noreply.github.com>
2023-08-26 09:43:29 +0530

    Add files via upload

---
+++ CODES/BASIC PROGRAMS/Multiplication.java
@@ -0,0 +1,12 @@
+class Multiplication
+{
+    public static void main(String args[])
+    {
+        int num=5,i;
+        System.out.println("Enter number:"+num);
+        for(i=1;i<=10;i++)
+        {
+            System.out.println(num+"*"+i+"="+num*i);
+        }
+    }
+}
