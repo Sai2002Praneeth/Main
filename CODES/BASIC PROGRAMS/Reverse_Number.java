c6d64bc9e66d3a09c6ef2c4436486d1e469c55ce
Sai Praneeth <110280913+Sai2002Praneeth@users.noreply.github.com>
2023-08-26 09:20:08 +0530

    Update Reverse_Number.java

--- CODES/BASIC PROGRAMS/Reverse_Number.java
+++ CODES/BASIC PROGRAMS/Reverse_Number.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class Number_Reverse {
+	
+	public static void main(String args[]){			 
+	        System.out.println("--- Reverse Number Program ---\n");
+	        System.out.print("Enter Number: ");
+	        Scanner in = new Scanner(System.in);
+	        int num = in.nextInt();
+	        // 0 error correction --important
+	        System.out.print("Reverse of  "+num+" is: ");
+
+	        System.out.println(revNumber(num));
+		in.close();		        
+	}
+	public static int revNumber(int num) {
+		if(num == 0) return 0;
+		long revNumber = 0;
+	        while(num > 0) {
+//		       	int rem = num%10;
+	        	revNumber = revNumber*10 + (num%10);
+	        	num /= 10;	        	
+	        }
+	        return (int)revNumber;
+	}	 
+}
